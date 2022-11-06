02a309a2813adc0cac0f6cecb33cf31f8e231c00
atikin19sh <atikin19sh@gmail.com>
2022-11-06 15:25:03 +0500

    Add react router navigation

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+import "./navigation.styles.scss";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          Logo
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="signIn">
+            Sign In
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,5 @@
+const SignIn = () => {
+  return <div>sign-in.component</div>;
+};
+
+export default SignIn;
