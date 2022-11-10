56089ca2f88e1b3d0445c6f5c52b8a4f72215af2
atikin19sh <atikin19sh@gmail.com>
2022-11-10 22:41:54 +0500

    add category preview feature and routes to every category

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
 import reportWebVitals from "./reportWebVitals";
 
-import App from "./App";
 import { UserProvider } from "./contexts/user.context";
+import { CartProvider } from "./contexts/cart.context";
 import { CategoriesProvider } from "./contexts/categories.context";
 
+import App from "./App";
+
 import "./index.scss";
-import { CartProvider } from "./contexts/cart.context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
