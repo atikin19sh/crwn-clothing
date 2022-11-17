b21d44fcfce22816d5b46ee9e0bb6809cad4129a
atikin19sh <atikin19sh@gmail.com>
2022-11-18 00:02:18 +0500

    convert categories thunk actions to saga

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,16 +1,21 @@
-import { useState, useEffect } from "react";
+import { Fragment, useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
-import { selectCategoriesMap } from "../../store/categories/categories.selector";
+import {
+  selectCategoriesIsLoading,
+  selectCategoriesMap,
+} from "../../store/categories/categories.selector";
 
 import ProductCard from "../../components/product-card/product-card.component";
+import Spinner from "../../components/spinner/spinner.component";
 
 import { CategoryContainer, Title } from "./category.styles";
 
 const Category = () => {
   const { category } = useParams();
   const categories = useSelector(selectCategoriesMap);
+  const categoriesIsLoading = useSelector(selectCategoriesIsLoading);
   const [products, setProducts] = useState(categories[category]);
 
   useEffect(() => {
@@ -19,13 +24,19 @@ const Category = () => {
 
   return (
     <main>
-      <Title>{category.toUpperCase()}</Title>
-      <CategoryContainer>
-        {products &&
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </CategoryContainer>
+      {categoriesIsLoading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <Title>{category.toUpperCase()}</Title>
+          <CategoryContainer>
+            {products &&
+              products.map((product) => (
+                <ProductCard key={product.id} product={product} />
+              ))}
+          </CategoryContainer>
+        </Fragment>
+      )}
     </main>
   );
 };
