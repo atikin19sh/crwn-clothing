c1a3578b67fcd20d8f70bd6bf48fe027f18f13c8
XeNoX <atikin19sh@gmail.com>
2022-11-25 17:19:38 +0500

    type category action + reducer

--- src/store/categories/categories.reducer.ts
+++ src/store/categories/categories.reducer.ts
@@ -1,5 +1,7 @@
 import { CATEGORIES_ACTION_TYPES } from "./categories.types";
 
+import { CategoryAction } from "./categories.action";
+
 const CATEGORIES_INITIAL_STATE = {
   categories: [],
   isLoading: false,
@@ -8,17 +10,15 @@ const CATEGORIES_INITIAL_STATE = {
 
 export const categoriesReducer = (
   state = CATEGORIES_INITIAL_STATE,
-  action = {},
+  action = {} as CategoryAction,
 ) => {
-  const { type, payload } = action;
-
-  switch (type) {
+  switch (action.type) {
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
       return { ...state, isLoading: true };
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
-      return { ...state, categories: payload, isLoading: false };
+      return { ...state, categories: action.payload, isLoading: false };
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
-      return { ...state, error: payload, isLoading: false };
+      return { ...state, error: action.payload, isLoading: false };
     default:
       return state;
   }
